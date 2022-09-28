0f6f36f60d318fadcc887b7809b2f6e7985bd961
hanaah1529 <114586364+hanaah1529@users.noreply.github.com>
2022-09-28 08:57:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Hanaa H"; // update the part BETWEEN the "quotes"
+let userName = "hanaah1529"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
